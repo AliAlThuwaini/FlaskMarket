7573e8dfaa43043abd82048e782a29113fadd92b
Ali <ali.althuwaini@outlook.com>
2022-02-13 22:03:55 +0300

    templates folder, home.html and double home route

--- market.py
+++ market.py
@@ -1,11 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+#double routes:
 @app.route('/')
-def hollo_world():
-  return '<h1> Hello, Wolrd!! Changed</h1>'
-
-#Dynamic routing
-@app.route('/about/<username>')
-def about_page(username):
-  return f'<h1> This is the about page of {username} </h1>'
+@app.route('/home')
+def home_page():
+  return render_template('home.html')
